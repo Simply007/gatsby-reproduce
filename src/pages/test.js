6414bdf8872a97ce4afeb02511567defe75061fc
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2022-12-14 08:54:34 +0100

    add test page

---
+++ src/pages/test.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+const TestPage = () => {
+  return (
+    <main>
+      <h1>Test page</h1>
+    </main>
+  );
+};
+
+export default TestPage;
+
+export const Head = () => <title>Home Page</title>;
