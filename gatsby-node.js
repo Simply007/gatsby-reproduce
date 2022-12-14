d84b9b0880a633308caf11f65576859eb71b4fb3
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2022-12-14 09:16:38 +0100

    add trailing slash

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,8 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
-    fromPath: `/articles`,
-    toPath: `/test`,
+    fromPath: `/articles/`,
+    toPath: `/test/`,
     redirectInBrowser: true,
     isPermanent: true,
   });
